5866012dc02ec7d63fc4124556f621ae9f35fc39
bk4206 <mdkhankjmaar@gmail.com>
2024-08-06 23:52:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def index():
  <head>
    <meta charset="utf-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Devil Server</title>
+   <title>Virat Roy</title>
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
    <style>
      body{
